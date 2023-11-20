fd875621f4680f5d6298c9c3eceddcc1c34a4bec
chinnawat99 <119986113+chinnawat99@users.noreply.github.com>
2023-11-20 14:28:26 +0700

    .commit

--- MyApp2.py
+++ MyApp2.py
@@ -10,8 +10,8 @@ col1, col2 = st.columns(2)
 #col2.write("This is column 2")
 with col1:
     st.image('./pic/Resignation.png')
-#with col2:
- #   st.image('./pic/Resignation.png')
+with col2:
+    st.image('./pic/Resignation2.png')
 
 html_1 = """
 <div style="background-color:#52BE80;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
