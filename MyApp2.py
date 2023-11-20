95fffb79146fd37a7fbc9c3841ce9f81ec9bfd25
chinnawat99 <119986113+chinnawat99@users.noreply.github.com>
2023-11-20 14:12:09 +0700

    .commit

---
+++ MyApp2.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.title('การทำนายการลาออกของพนักงาน')
+st.header('นาย ชินวัฒน์ ภูไชยแสง')
+st.subheader('สาขาวิชาวิทยาการข้อมูล')
+st.markdown("----")
+
+col1, col2 = st.columns(2)
+#col1.write("This is column 1")
+#col2.write("This is column 2")
+with col1:
+    #st.image('./pic/rung1.jfif')
+with col2:
+    #st.image('./pic/iris2.jpg')
+
+html_1 = """
+<div style="background-color:#52BE80;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>ข้อมูลของพนักงาน</h5></center>
+</div>
+"""
+st.markdown(html_1, unsafe_allow_html=True)
+st.markdown("")
+
